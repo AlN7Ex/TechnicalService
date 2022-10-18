bc90b59da078f713ce2e556441f50ba1158dfc85
Alexey <demon1c1@rambler.ru>
2022-10-18 10:33:19 +0300

    Init project

---
+++ src/test/java/com/example/springboothiber/SpringBootHiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHiberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
